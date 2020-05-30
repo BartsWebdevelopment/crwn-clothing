5e75567d721255095a12d52500eb1c537ac31392
Bart <bartswebdevelopment@gmail.com>
2020-05-30 10:32:02 +0200

    sass geinstalleerd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div>
+            <HomePage />
+        </div>
+);
 }
 
 export default App;

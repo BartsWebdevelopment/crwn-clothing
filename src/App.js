43ca1f3af57e7f14d3287f437d8f62850959067c
Bart <bartswebdevelopment@gmail.com>
2020-05-31 14:01:11 +0200

    Lesson #72 Shop page: updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from "./components/shop/shop.component";
+import ShopPage from "./pages/shop/shop.component";
 
 
 function App() {
